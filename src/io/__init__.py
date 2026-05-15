951feb5c3044532298cc64b5c7875b7ef4484449
Beyza Betül AY <beyzabetuel.ay@study.thws.de>
2026-05-15 20:20:48 +0200

    chore: export video loader API from io package
    Refs #6

--- src/io/__init__.py
+++ src/io/__init__.py
@@ -4,12 +4,20 @@ from src.io.media_loader import (
     ImageInput,
     ImageLoadError,
     UnsupportedFormatError,
+    VideoInfo,
+    VideoLoadError,
+    get_video_info,
     load_image,
+    load_video_frames,
 )
 
 __all__ = [
     "ImageInput",
     "ImageLoadError",
     "UnsupportedFormatError",
+    "VideoInfo",
+    "VideoLoadError",
+    "get_video_info",
     "load_image",
+    "load_video_frames",
 ]
